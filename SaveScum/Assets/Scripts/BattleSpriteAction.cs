7783d9919971439f99b8a8a0d7b3cf53e6e9c933
WhiteRavensGame <cuecharles@gmail.com>
2025-07-31 11:11:11 -0700

    Deleted older unity chan platformer version

---
+++ SaveScum/Assets/Scripts/BattleSpriteAction.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BattleSpriteAction : MonoBehaviour
+{
+	static int hashSpeed = Animator.StringToHash ("Speed");
+	static int hashFallSpeed = Animator.StringToHash ("FallSpeed");
+	static int hashGroundDistance = Animator.StringToHash ("GroundDistance");
+	static int hashIsCrouch = Animator.StringToHash ("IsCrouch");
+	static int hashAttack1 = Animator.StringToHash ("Attack1");
+	static int hashAttack2 = Animator.StringToHash ("Attack2");
+	static int hashAttack3 = Animator.StringToHash ("Attack3");
+
+
+	static int hashDamage = Animator.StringToHash ("Damage");
+	static int hashIsDead = Animator.StringToHash ("IsDead");
+
+	[SerializeField] private float characterHeightOffset = 0.2f;
+	[SerializeField] LayerMask groundMask;
+
+	[SerializeField, HideInInspector] Animator animator;
+	[SerializeField, HideInInspector]SpriteRenderer spriteRenderer;
+	[SerializeField, HideInInspector]Rigidbody2D rig2d;
+
+	public int hp = 4;
+
+	private bool canJump = true;
+	private bool isAlive = true;
+
+	void Awake ()
+	{
+		animator = GetComponent<Animator> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		rig2d = GetComponent<Rigidbody2D> ();
+	}
+
+    void Start()
+    {
+		GameManager gm = GameManager.Instance;
+    }
+
+    void Update ()
+	{
+		if (!isAlive)
+			return;
+		
+
+		float axis = Input.GetAxisRaw ("Horizontal");
+		bool isDown = Input.GetAxisRaw ("Vertical") < 0;
+
+        Vector2 velocity = rig2d.linearVelocity;
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            velocity.y = 5;
+            canJump = false;
+        }
+        if (axis != 0)
+        {
+            spriteRenderer.flipX = axis < 0;
+            velocity.x = axis * 2;
+        }
+        rig2d.linearVelocity = velocity;
+
+        var distanceFromGround = Physics2D.Raycast (transform.position, Vector3.down, 1, groundMask);
+
+		// update animator parameters
+		animator.SetBool (hashIsCrouch, isDown);
+		animator.SetFloat (hashGroundDistance, distanceFromGround.distance == 0 ? 99 : distanceFromGround.distance - characterHeightOffset);
+		animator.SetFloat (hashFallSpeed, rig2d.linearVelocity.y);
+		animator.SetFloat (hashSpeed, Mathf.Abs (axis));
+		if( Input.GetKeyDown(KeyCode.Z) ){  animator.SetTrigger(hashAttack1); }
+		if( Input.GetKeyDown(KeyCode.X) ){  animator.SetTrigger(hashAttack2); }
+		if( Input.GetKeyDown(KeyCode.C) ){  animator.SetTrigger(hashAttack3); }
+
+		//// flip sprite
+		//if (axis != 0)
+		//	spriteRenderer.flipX = axis < 0;
+
+        //QQQQ hardcoded jump
+        if (animator.GetFloat("GroundDistance") < 0.2f && animator.GetFloat("FallSpeed") < 0)
+        {
+            canJump = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("DamageObject"))
+		{
+			HurtPlayer();
+			Console.WriteLine("Damage player");
+		}
+    }
+
+	private void HurtPlayer(int damage = 999)
+	{
+		hp -= damage;
+		if(hp <= 0)
+		{
+			KillPlayer();
+		}
+
+        animator.SetTrigger(hashDamage);
+    }
+
+	private void KillPlayer()
+	{
+        animator.SetBool(hashIsDead, true);
+        isAlive = false;
+		
+    }
+
+}
---
+++ SaveScum/Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [SerializeField]
+    private float maxTimeLeft = 5f;
+    private float timeLeft;
+
+    private GameState gameState = GameState.Playing;
+
+    public event EventHandler OnTimeUp;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(this.gameObject);
+    }
+
+    private void Start()
+    {
+        timeLeft = maxTimeLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameState == GameState.Playing)
+        {
+            timeLeft -= Time.deltaTime;
+            if(timeLeft <= 0)
+            {
+                gameState = GameState.GameOver;
+                OnTimeUp.Invoke(this, EventArgs.Empty);
+            }
+
+        }
+            
+    }
+}
+
+public enum GameState
+{
+    None,
+    Playing,
+    GameOver,
+    Intermission
+}
